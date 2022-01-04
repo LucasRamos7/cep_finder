594a8b374df58ab8653aa9182f26d8610a21e706
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2022-01-03 21:29:25 -0300

    Tratamento de erro implementado

--- cep_finder.py
+++ cep_finder.py
@@ -1,31 +1,33 @@
 import requests
-import re
 
 
 def main():
 
-    padrao_cep= re.compile('[0-9]{5}-?[0-9]{3}')
-
-    cep = input('Insira o CEP: ')
-
-    if padrao_cep.fullmatch(cep):
-        cep = cep.strip('-')
-    else:
-        raise ValueError('CEP inválido!')
+    cep = input('Insira o CEP: ').strip('-')
 
     r = requests.get(url=f'https://viacep.com.br/ws/{cep}/json')
 
+    while r.status_code == 400:
+        print('\nCEP inválido!\n')
+
+        cep = input('Insira o CEP: ').strip('-')
+
+        r = requests.get(url=f'https://viacep.com.br/ws/{cep}/json')
+
     resposta = r.json()
 
-    cep_resposta = resposta['cep']
-    logradouro = resposta['logradouro']
-    complemento = resposta['complemento'] or 's/ complemento'
-    bairro = resposta['bairro']
-    cidade = resposta['localidade']
-    uf = resposta['uf']
+    if 'erro' not in resposta:
+        cep_resposta = resposta['cep']
+        logradouro = resposta['logradouro']
+        complemento = resposta['complemento'] or 's/ complemento'
+        bairro = resposta['bairro']
+        cidade = resposta['localidade']
+        uf = resposta['uf']
 
-    print(f'O CEP {cep_resposta} corresponde a: {logradouro}, {complemento},'
-          f' no bairro {bairro}, município {cidade} ({uf})')
+        print(f'\nO CEP {cep_resposta} corresponde a: {logradouro}, {complemento},'
+              f' no bairro {bairro}, município {cidade} ({uf})\n')
+    else:
+        print('CEP inexistente!')
 
 
 if __name__ == '__main__':
