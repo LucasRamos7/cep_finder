6aebdf06d0d2acca73665156f81c47c6d5e7074b
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2022-01-07 20:47:07 -0300

    Busca por CEP completa

--- cep_finder.py
+++ cep_finder.py
@@ -1,10 +1,39 @@
 import requests
 
 
+def encontrar_cep():
+    uf = input('Insira a UF (ex: RJ): ').upper()
+    cidade = input('Insira o nome da cidade: ')
+    logradouro = input('Insira o nome do logradouro (ex: Presidente Vargas): ')
+
+    r = requests.get(url=f'https://viacep.com.br/ws/{uf}/{cidade}/{logradouro}/json')
+
+
 def main():
 
+    print('Qual das opções se adequa melhor a você?')
+    print('(1) Tenho um CEP e quero descobrir o endereço')
+    print('(2) Tenho um endereço e preciso descobrir seu CEP\n\n')
+
+    opcao = int(input())
+
+    if opcao == 1:
+        encontrar_endereco()
+    elif opcao == 2:
+        encontrar_cep()
+    else:
+        print('Opção inválida!')
+
+
+def encontrar_endereco():
     cep = input('Insira o CEP: ').strip('-')
 
+    resposta = faz_request(cep)
+
+    imprime_endereco(resposta)
+
+
+def faz_request(cep):
     r = requests.get(url=f'https://viacep.com.br/ws/{cep}/json')
 
     while r.status_code == 400:
@@ -15,7 +44,10 @@ def main():
         r = requests.get(url=f'https://viacep.com.br/ws/{cep}/json')
 
     resposta = r.json()
+    return resposta
 
+
+def imprime_endereco(resposta):
     if 'erro' not in resposta:
         cep_resposta = resposta['cep']
         logradouro = resposta['logradouro']
